79777e39d6a29c996e4db9ef2adebcb95ae55699
Jeza <jeza@vip.qq.com>
2017-09-14 20:47:23 +0800

    Update#20170914#2033#稍微改动一下

--- Common/Share.h
+++ Common/Share.h
@@ -1,13 +1,14 @@
 //
 // Created by Jeza on 2017/9/14.
 //
-// 访问器
+
 #ifndef LIST_VISIT_H
 #define LIST_VISIT_H
 
 #include <iostream>
 using namespace std;
 
+// 访问器
 template<typename T>
 class PRINTER
 {
@@ -17,3 +18,7 @@ class PRINTER
     }
 };
 #endif //LIST_VISIT_H
+
+//安全释放器
+template<typename T>
+void release(T* x) {if(x) delete x;}
