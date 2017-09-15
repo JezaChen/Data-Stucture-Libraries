619af39ccbb189ea5950ff7b55243d70ed125137
Jeza <jeza@vip.qq.com>
2017-09-15 18:28:47 +0800

    Update#20170915#1828#增加了无序向量Vector，并且增加了判等器和比较器机制

--- Common/Share.h
+++ Common/Share.h
@@ -16,9 +16,17 @@ namespace DSLibrary {
         }
     };
 
-#endif //LIST_VISIT_H
-
 //安全释放器
     template<typename T>
     void release(T *x) { if (x) delete x; }
-}
+
+//判等器、比较器
+    template<typename T> static bool lt (T& a, T& b) {return  a<b;}
+    template<typename T> static bool lt (T* a, T* b) {return  lt(*a,*b);}
+    template<typename T> static bool rt (T& a, T& b) {return  a>b;}
+    template<typename T> static bool rt (T* a, T* b) {return  rt(*a,*b);}
+    template<typename T> static bool eq (T& a, T& b) {return a==b;}
+    template<typename T> static bool eq (T* a ,T* b) {return eq(*a,*b);}
+    }
+#endif //LIST_VISIT_H
+
--- List/AList.h
+++ List/AList.h
@@ -5,7 +5,6 @@
 
 #ifndef LIST_ALIST_H
 #define LIST_ALIST_H
-
 namespace DSLibrary {
 
     template<typename T>
@@ -17,28 +16,23 @@ namespace DSLibrary {
         T *data; //储存列表数据的数组
     public:
         Alist(int s = 250) : capacity(s) { data = new T[s]; }
-
         void clear(); //清空
         void append(const T &); //追加
         void insert(const T &); //插入
         T remove(); //删除
 
         //位置操作
-        void moveToStart(); //移到最前端
-        void moveToEnd(); //移到最后端
-        void prev(); //向前走一位
-        void next(); //向后走一位
-
-        void moveToPosi(int posi); //移动到某个位置
-        const T &getValue(); //获取当前位置的值
+        void moveToStart();   //移到最前端
+        void moveToEnd();   //移到最后端
+        void prev();   //向前走一位
+        void next();   //向后走一位
+        void moveToPosi(int posi);   //移动到某个位置
+        const T &getValue();   //获取当前位置的值
         T &operator[](int posi);
+        int currentPosi();   //返回当前位置
 
-        int currentPosi(); //返回当前位置
-
-        int length() const //表的长度
-        {
-            return listSize;
-        }
+        int length() const { return listSize; }  //表的长度
+        bool is_empty()const {return !listSize;}   //表是否为空
 
         //遍历操作
         template<typename VST> void trav(VST& visit);
