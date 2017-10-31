fd03d4c5271e65b4c98c28396118325e2bf708e5
Chen Jianzhang <jeza@vip.qq.com>
2017-11-01 07:02:58 +0800

    20171101#0702#增增添添

---
+++ Tree/tree.h
@@ -0,0 +1,47 @@
+//
+// Created by jeza on 17-10-31.
+//
+
+#ifndef DSL_TREE_H
+#define DSL_TREE_H
+
+
+#define GTNodePosi(T) GeneralTreeNode<T>*
+namespace DSLibrary
+{
+
+    template<typename T>
+    class GeneralTreeNode
+        {
+        public:
+            virtual T data() = 0;
+
+            virtual void setData(const T &elem) = 0;
+
+            virtual GTNodePosi(T)leftMostChild() = 0;
+
+            virtual GTNodePosi(T)rightSibling() = 0;
+
+            virtual void insertAsFirstChild(const T &elem) = 0;
+
+            virtual void insertAsRightSibling(const T &elem) = 0;
+
+            virtual T removeFirstChild() = 0;
+
+            virtual T removeRightSibling() = 0;
+        };
+
+    template<typename T>
+    class GeneralTree
+        {
+        public:
+            virtual void clear() = 0;
+
+            virtual GTNodePosi(T)root() = 0;
+
+            virtual void insertAsRoot(const T &, GTNodePosi(T), GTNodePosi(T)) = 0;
+
+            virtual void print() = 0;
+        };
+}
+#endif //DSL_TREE_H
--- uniPRINTER/print.h
+++ uniPRINTER/print.h
@@ -16,43 +16,70 @@
 #include "../List/Link.h"
 #include "../List/List.h"
 #include "../List/Doubly_Linked_List.h"
+#include "../Tree/tree.h"
 
 
 using namespace std;
 
-namespace DSLibrary {
+namespace DSLibrary
+{
 
-    template<typename T> static void print(T* x);
-    template<typename T> static void print(T& x);
-    template<typename T> static void print(const T&x);
+    template<typename T>
+    static void print(T *x);
+
+    template<typename T>
+    static void print(T &x);
+
+    template<typename T>
+    static void print(const T &x);
 
     //内接口
     class UNI_PRINTER //统一输出类
-    {
-    public:
-        //基本类型打印
-        static void p(const int &x) {printf("%d  ",x);}
-        static void p(const float &x) {printf("%.2f  ",x);}
-        static void p(const double &x) {printf("%2f  ",x);}
-        static void p(const char &x) {printf ( " %c",(31<x)&&(x<128)?x:'$');}
-
-        //复杂数据结构的打印输出
-        template<typename T> static void p(List<T>& list)
         {
-            list.trav(print);
-        }
+        public:
+            //基本类型打印
+            static void p(const int &x)
+            { printf("%d  ", x); }
 
-    };
+            static void p(const float &x)
+            { printf("%.2f  ", x); }
+
+            static void p(const double &x)
+            { printf("%2f  ", x); }
+
+            static void p(const char &x)
+            { printf(" %c", (31 < x) && (x < 128) ? x : '$'); }
+
+            //复杂数据结构的打印输出
+            template<typename T>
+            static void p(List<T> &list)
+            {
+                list.trav(print);
+            }
+
+            template<typename T>
+            static void p(GeneralTree<T> &list)
+            {
+                //int a = 5;
+            }
+
+        };
 
     //对外接口的实现
-    template<typename T> static void print(T* x)
+    template<typename T>
+    static void print(T *x)
     {
-        if(x) print(*x);
+        if (x) print(*x);
         else printf("<NULL>");
     } //指针访问
     //引用访问
-    template<typename T> static void print(T& x) {UNI_PRINTER::p(x);}
-    template<typename T> static void print(const T&x) {UNI_PRINTER::p(x);}
+    template<typename T>
+    static void print(T &x)
+    { UNI_PRINTER::p(x); }
+
+    template<typename T>
+    static void print(const T &x)
+    { UNI_PRINTER::p(x); }
 
 }
 #endif //UNIPRINTER_PRINT_H
